1ac85cec54199de942c9881a98aac2d0b60d6ef1
Klaus Kazlauskas <klausmachado@gmail.com>
2018-12-31 16:56:40 -0200

    refactor(environments): replace old environment logic
    Previously EnvironmentProvider was called EnvironmentHolder and
    EnvironmentHolder was called EnvironmentSharerService.

--- environments/projects/provider/src/app/displayer/displayer.component.ts
+++ environments/projects/provider/src/app/displayer/displayer.component.ts
@@ -1,15 +1,15 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import { environment } from '../../environments/environment';
-import { ENVIRONMENT, EnvironmentHolder, EnvironmentSharerService } from 'sharer';
+import { ENVIRONMENT, EnvironmentProvider, EnvironmentHolder } from 'sharer';
 
 @Component({
   selector: 'app-displayer',
   template: `
     <h2>Displayer</h2>
     <p>Provider application environment: {{environment}}</p>
-    <p>Injected environemnt: {{injectedEnvironment.title}}</p>
-    <p>Environment holder: {{environmentHolder.injectedEnvironment.title}}</p>
-    <p>Environment sharer: {{environmentSharer.getEnvironment() | json:2}}</p>
+    <p>Injected environment: {{injectedEnvironment.title}}</p>
+    <p>Environment provider: {{environmentProvider.injectedEnvironment.title}}</p>
+    <p>Environment holder: {{environmentHolder.getEnvironment() | json:2}}</p>
   `
 })
 export class DisplayerComponent implements OnInit {
@@ -18,8 +18,8 @@ export class DisplayerComponent implements OnInit {
 
   constructor(
     @Inject(ENVIRONMENT) public injectedEnvironment: any,
-    public environmentHolder: EnvironmentHolder,
-    public environmentSharer: EnvironmentSharerService
+    public environmentProvider: EnvironmentProvider,
+    public environmentHolder: EnvironmentHolder
   ) {}
 
   ngOnInit() {
