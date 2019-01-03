c39fc6661306eddaee008174fcfb44a11fa8d6f1
Klaus Kazlauskas <klausmachado@gmail.com>
2019-01-02 23:11:49 -0200

    feat(environments): add global environment files

---
+++ environments/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import { EnvironmentId } from 'sharer';
+
+export const environment = {
+  production: true,
+  title: 'Global Prod Development',
+  id: EnvironmentId.Provider
+};
---
+++ environments/environments/environment.ts
@@ -0,0 +1,7 @@
+import { EnvironmentId } from 'sharer';
+
+export const environment = {
+  production: false,
+  title: 'Global Development',
+  id: EnvironmentId.Provider
+};
