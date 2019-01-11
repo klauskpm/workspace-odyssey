c1d03dc82cb338869b4dd52fda27e1f6c5f97edc
Klaus Kazlauskas <klausmachado@gmail.com>
2019-01-10 23:55:24 -0200

    feat(environments): add angular material to sun app

--- environments/projects/sun/src/app/app.module.ts
+++ environments/projects/sun/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EnvironmentDisplayerModule } from './environment-displayer/environment-displayer.module';
+import { MatToolbarModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    EnvironmentDisplayerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
