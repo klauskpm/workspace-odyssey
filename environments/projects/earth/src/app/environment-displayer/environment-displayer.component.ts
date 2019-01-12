29137d16106ecf6d63e3c3376f35395bc93052fa
Klaus Kazlauskas <klausmachado@gmail.com>
2019-01-12 11:13:34 -0200

    refactor(environments): add environments to display

--- environments/projects/earth/src/app/environment-displayer/environment-displayer.component.ts
+++ environments/projects/earth/src/app/environment-displayer/environment-displayer.component.ts
@@ -1,37 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 import { EnvironmentId } from 'satellite';
-import { from, Observable } from 'rxjs';
+import { from, Observable, of, zip } from 'rxjs';
 import { map, switchMap, toArray } from 'rxjs/operators';
+import { Environmenter } from 'satellite';
 
 @Component({
   selector: 'app-environment-displayer',
   template: `
-    <mat-chip-list *ngFor="let environments of environmentsList$ | async">
-      <mat-chip
-        *ngFor="let environment of environments"
-        [color]="environment.color"
-        [selected]="environment.selected"
-      >{{environment.label}}: {{environment.title}}</mat-chip>
-    </mat-chip-list>
+    <div *ngFor="let environment of environmentsList$ | async">
+      <h2>{{environment.title}}</h2>
+      <mat-chip-list>
+        <mat-chip
+          *ngFor="let data of environment.data"
+          [color]="data.color"
+          [selected]="data.selected"
+        >{{data.label}}: {{data.title}}</mat-chip>
+      </mat-chip-list>
+    </div>
   `,
   styles: []
 })
 export class EnvironmentDisplayerComponent implements OnInit {
-  public environmentsList$: Observable<Array<Array<any>>>;
+  public environmentsList$: Observable<Array<any>>;
   private environmentColors = {
     [EnvironmentId.Development]: 'primary',
     [EnvironmentId.Production]: 'accent'
   };
 
-  constructor() { }
+  constructor(
+    private environmenter: Environmenter
+  ) { }
 
   ngOnInit() {
+    this.environmentsList$ = this.transformEnvironments([
+      {
+        title: 'For Root',
+        data: this.environmenter.getForRootEnvironments(),
+      },
+      {
+        title: 'Function',
+        data: this.environmenter.getFunctionEnvironments(),
+      },
+      {
+        title: 'Provider',
+        data: this.environmenter.getProviderEnvironments(),
+      }
+    ]);
   }
 
-  private transformEnvironments(environmentsList: Array<Array<any>>): Observable<Array<Array<any>>> {
+  private transformEnvironments(environmentsList: Array<any>): Observable<Array<any>> {
     return from(environmentsList)
       .pipe(
-        switchMap((environments: Array<any>) => this.addMatChipProperties(environments)),
+        switchMap((environments: any) => {
+          const title$ = of(environments.title);
+          const dataWithMatChipProperties$ = this.addMatChipProperties(environments.data);
+          return zip(title$, dataWithMatChipProperties$);
+        }),
+        map(([title, data]) => ({title, data})),
         toArray()
       );
   }
