212c68211593d08428c14e52b57aa591639ce261
Klaus Kazlauskas <klausmachado@gmail.com>
2019-01-12 14:30:16 -0200

    feat(environments): add material to moon application

--- environments/projects/moon/src/app/app.module.ts
+++ environments/projects/moon/src/app/app.module.ts
@@ -2,15 +2,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material';
+import { EnvironmentDisplayerModule } from '../../../earth/src/app/environment-displayer/environment-displayer.module';
+import { environment } from '../environments/environment';
+import {
+  DIRECT_PROVIDER,
+  DOUBLE_EXTENDED_PROVIDER,
+  EnvironmenterModule,
+  EXTENDED_PROVIDER,
+  injectToken
+} from 'satellite';
+
+const provider = { value: 'provider' };
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    EnvironmentDisplayerModule,
+    EnvironmenterModule.forRoot(environment)
   ],
-  providers: [],
+  providers: [
+    ...injectToken(environment),
+    {
+      provide: DIRECT_PROVIDER,
+      useValue: environment
+    },
+    {
+      provide: EXTENDED_PROVIDER,
+      useValue: { ...environment, provider }
+    },
+    {
+      provide: DOUBLE_EXTENDED_PROVIDER,
+      useValue: { ...environment, ...provider }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
