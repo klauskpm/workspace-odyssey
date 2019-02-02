a32a82292c3ba88178aae13ef380dc9858e85c35
Klaus Kazlauskas <klausmachado@gmail.com>
2019-02-02 11:10:18 -0200

    feat(styles): create stellite library

---
+++ styles/projects/satellite/src/lib/satellite.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { SatelliteComponent } from './satellite.component';
+
+@NgModule({
+  declarations: [SatelliteComponent],
+  imports: [
+  ],
+  exports: [SatelliteComponent]
+})
+export class SatelliteModule { }
