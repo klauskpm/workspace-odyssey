016dcd1b56ab641660882e8d7b462e1901f073d9
Klaus Kazlauskas <klausmachado@gmail.com>
2019-02-05 19:24:28 -0200

    feat(file-sharing): create earth component

--- file-sharing/projects/earth/src/app/app.module.ts
+++ file-sharing/projects/earth/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EarthComponent } from './earth/earth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EarthComponent
   ],
   imports: [
     BrowserModule
---
+++ file-sharing/projects/earth/src/app/earth/earth.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-earth',
+  template: `
+    <p>
+      earth works!
+    </p>
+  `,
+  styles: []
+})
+export class EarthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
