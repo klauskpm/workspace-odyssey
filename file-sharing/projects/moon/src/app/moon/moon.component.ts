f8646bd0c142d72b622d3e6f643f74ebc16aceb8
Klaus Kazlauskas <klausmachado@gmail.com>
2019-02-05 19:25:25 -0200

    feat(file-sharing): create moon component

---
+++ file-sharing/projects/moon/src/app/moon/moon.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-moon',
+  template: `
+    <p>
+      moon works!
+    </p>
+  `,
+  styles: []
+})
+export class MoonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
